285e5130b18d663f31d4c0a8a4bbadc42e876c6f
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:14:54 +0000

    Reset to e861235d28d5598fac82c90aaec11c9d4218cc3c and made changes for LLM Round 1

--- tests/test_velocity_limit.py
+++ tests/test_velocity_limit.py
@@ -7,6 +7,7 @@ from robot_descriptions.loaders.mujoco import load_robot_description
 
 from mink import Configuration
 from mink.limits import LimitDefinitionError, VelocityLimit
+from mink.utils import get_freejoint_dims
 
 
 class TestVelocityLimit(absltest.TestCase):
@@ -18,21 +19,28 @@ class TestVelocityLimit(absltest.TestCase):
 
     def setUp(self):
         self.configuration = Configuration(self.model)
-        self.configuration.update_from_keyframe("home")
+        self.configuration.update_from_keyframe("stand")
         self.velocities = {
-            "shoulder_pan_joint": np.pi,
-            "shoulder_lift_joint": np.pi,
-            "elbow_joint": np.pi,
-            "wrist_1_joint": np.pi,
-            "wrist_2_joint": np.pi,
-            "wrist_3_joint": np.pi,
+            self.model.joint(i).name: [3.14] * self.model.joint(i).dof for i in range(self.model.njnt)
         }
+
+    def test_throws_error_if_gain_invalid(self):
+        with self.assertRaises(LimitDefinitionError):
+            VelocityLimit(self.model, gain=-1)
+        with self.assertRaises(LimitDefinitionError):
+            VelocityLimit(self.model, gain=1.1)
 
     def test_dimensions(self):
         limit = VelocityLimit(self.model, self.velocities)
         nv = self.configuration.nv
-        self.assertTrue(np.array_equal(limit.projection_matrix.shape, (nv, nv)))
-        self.assertEqual(len(limit.indices), nv)
+        nb = nv - len(get_freejoint_dims(self.model)[1])
+        self.assertEqual(len(limit.indices), nb)
+        self.assertEqual(limit.projection_matrix.shape, (nb, nv))
+
+    def test_indices(self):
+        limit = VelocityLimit(self.model, self.velocities)
+        expected = np.arange(6, self.model.nv)  # Freejoint (0-5) is not limited.
+        self.assertTrue(np.array_equal(limit.indices, expected))
 
     def test_model_with_no_limit(self):
         empty_model = mujoco.MjModel.from_xml_string("<mujoco></mujoco>")
@@ -45,62 +53,14 @@ class TestVelocityLimit(absltest.TestCase):
 
     def test_model_with_subset_of_velocities_limited(self):
         velocities = {
-            "wrist_1_joint": np.pi,
-            "wrist_2_joint": np.pi,
-            "wrist_3_joint": np.pi,
+            self.model.joint(i).name: [3.14] * self.model.joint(i).dof
+            for i in range(self.model.njnt) if self.model.joint(i).dof == 1
         }
         limit = VelocityLimit(self.model, velocities)
-        nb = 3
+        nb = sum(self.model.joint(i).dof == 1 for i in range(self.model.njnt))
         nv = self.model.nv
-        self.assertTrue(np.array_equal(limit.projection_matrix.shape, (nb, nv)))
+        self.assertEqual(limit.projection_matrix.shape, (nb, nv))
         self.assertEqual(len(limit.indices), nb)
-
-    def test_model_with_ball_joint(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body>
-              <joint type="ball" name="ball"/>
-              <geom type="sphere" size=".1" mass=".1"/>
-              <body>
-                <joint type="hinge" name="hinge" range="0 1.57"/>
-                <geom type="sphere" size=".1" mass=".1"/>
-              </body>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
-        model = mujoco.MjModel.from_xml_string(xml_str)
-        velocities = {
-            "ball": (np.pi, np.pi / 2, np.pi / 4),
-            "hinge": (0.5,),
-        }
-        limit = VelocityLimit(model, velocities)
-        nb = 3 + 1
-        self.assertEqual(len(limit.indices), nb)
-        self.assertTrue(np.array_equal(limit.projection_matrix.shape, (nb, model.nv)))
-
-    def test_ball_joint_invalid_limit_shape(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body>
-              <joint type="ball" name="ball"/>
-              <geom type="sphere" size=".1" mass=".1"/>
-              <body>
-                <joint type="hinge" name="hinge" range="0 1.57"/>
-                <geom type="sphere" size=".1" mass=".1"/>
-              </body>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
-        model = mujoco.MjModel.from_xml_string(xml_str)
-        velocities = {
-            "ball": (np.pi, np.pi / 2),
-        }
-        with self.assertRaises(LimitDefinitionError):
-            VelocityLimit(model, velocities)
 
     def test_that_freejoint_raises_error(self):
         xml_str = """
@@ -110,7 +70,7 @@ class TestVelocityLimit(absltest.TestCase):
               <joint type="free" name="floating"/>
               <geom type="sphere" size=".1" mass=".1"/>
               <body>
-                <joint type="hinge" name="hinge" range="0 1.57" limited="true"/>
+                <joint type="hinge" name="hinge" range="0 1.57"/>
                 <geom type="sphere" size=".1" mass=".1"/>
               </body>
             </body>
@@ -119,11 +79,20 @@ class TestVelocityLimit(absltest.TestCase):
         """
         model = mujoco.MjModel.from_xml_string(xml_str)
         velocities = {
-            "floating": np.pi,
-            "hinge": np.pi,
+            "floating": [np.pi, np.pi / 2, np.pi / 4],
+            "hinge": [0.5],
         }
         with self.assertRaises(LimitDefinitionError):
             VelocityLimit(model, velocities)
+
+    def test_indices_of_limited_velocities(self):
+        velocities = {
+            self.model.joint(i).name: [3.14] * self.model.joint(i).dof
+            for i in range(self.model.njnt) if self.model.joint(i).dof == 1
+        }
+        limit = VelocityLimit(self.model, velocities)
+        expected_indices = np.array([j.id for j in self.model.joints if j.dof == 1])
+        self.assertTrue(np.array_equal(limit.indices, expected_indices))
 
 
 if __name__ == "__main__":
