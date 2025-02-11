16644d35fbb29c6c3c3e92c2316176025bd7b68f
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:29:19 +0000

    Reset to a69629a7fbeccaa8b60c30e9a385b9c1de01e3cf and made changes for LLM Round 1

--- tests/test_collision_avoidance_limit.py
+++ tests/test_collision_avoidance_limit.py
@@ -1,12 +1,10 @@
 """Tests for configuration_limit.py."""
 
-import itertools
-
+import mujoco
 import numpy as np
 from absl.testing import absltest
 from robot_descriptions.loaders.mujoco import load_robot_description
 
-from mink import Configuration
 from mink.limits import CollisionAvoidanceLimit
 from mink.utils import get_body_geom_ids
 
@@ -33,27 +31,17 @@ class TestCollisionAvoidanceLimit(absltest.TestCase):
             bound_relaxation=bound_relaxation,
         )
 
-        # Check that non-colliding geoms are correctly filtered out and that we have
-        # the right number of max expected contacts.
         g1_coll = [
-            g
-            for g in g1
-            if self.model.geom_conaffinity[g] != 0 and self.model.geom_contype[g] != 0
+            g for g in g1 if self.model.geom_conaffinity[g] != 0 and self.model.geom_contype[g] != 0
         ]
         g2_coll = [
-            g
-            for g in g2
-            if self.model.geom_conaffinity[g] != 0 and self.model.geom_contype[g] != 0
+            g for g in g2 if self.model.geom_conaffinity[g] != 0 and self.model.geom_contype[g] != 0
         ]
         expected_max_num_contacts = len(list(itertools.product(g1_coll, g2_coll)))
         self.assertEqual(limit.max_num_contacts, expected_max_num_contacts)
 
         G, h = limit.compute_qp_inequalities(self.configuration, 1e-3)
-
-        # The upper bound should always be >= relaxation bound.
         self.assertTrue(np.all(h >= bound_relaxation))
-
-        # Check that the inequality constraint dimensions are valid.
         self.assertEqual(G.shape, (expected_max_num_contacts, self.model.nv))
         self.assertEqual(h.shape, (expected_max_num_contacts,))
 
@@ -74,7 +62,12 @@ class TestCollisionAvoidanceLimit(absltest.TestCase):
             bound_relaxation=bound_relaxation,
         )
 
-        G_mujoco, h_mujoco = limit.compute_qp_inequalities(self.configuration, 1e-3)
+        # Initialize MuJoCo data object
+        data = mujoco.MjData(self.model)
+        mujoco.mj_forward(self.model, data)
+
+        # Compute contact normal Jacobian using the limit
+        G_mujoco, h_mujoco = limit.compute_contact_normal_jacobian(data, bound_relaxation)
 
         # Reset MuJoCo options to default
         self.model.opt.cone = 1
