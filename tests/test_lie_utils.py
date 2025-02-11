fc105e6de6bec9f386a8ab508993b2ca0d4a7871
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:45:54 +0000

    Reset to 372c0d0807895860f5f90ac4cc1d0cae7b98f14c and made changes for LLM Round 1

--- tests/test_lie_utils.py
+++ tests/test_lie_utils.py
@@ -7,8 +7,8 @@ from mink.lie import utils
 
 
 class TestUtils(absltest.TestCase):
-    def test_skew_throws_assertion_error_if_shape_invalid(self):
-        with self.assertRaises(AssertionError):
+    def test_skew_throws_value_error_if_shape_invalid(self):
+        with self.assertRaises(ValueError):
             utils.skew(np.zeros((5,)))
 
     def test_skew_equals_negative(self):
