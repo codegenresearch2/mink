137fcc505d7ded76273355a6ba8ae37cdf9b96b5
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:14:39 +0000

    Reset to a69629a7fbeccaa8b60c30e9a385b9c1de01e3cf and made changes for LLM Round 1

--- tests/test_lie_operations.py
+++ tests/test_lie_operations.py
@@ -4,6 +4,7 @@ from typing import Type
 
 import numpy as np
 from absl.testing import absltest, parameterized
+import pytest
 
 from mink.lie.base import MatrixLieGroup
 from mink.lie.se3 import SE3
@@ -12,72 +13,74 @@ from mink.lie.so3 import SO3
 from .utils import assert_transforms_close
 
 
-def test_inverse_bijective(group: Type[MatrixLieGroup]):
-    """Check inverse of inverse."""
-    transform = group.sample_uniform()
-    assert_transforms_close(transform, transform.inverse().inverse())
+@pytest.fixture(params=[SO3, SE3])
+def group(request):
+    return request.param
 
 
-def test_matrix_bijective(group: Type[MatrixLieGroup]):
-    """Check that we can convert to and from matrices."""
-    transform = group.sample_uniform()
-    assert_transforms_close(transform, group.from_matrix(transform.as_matrix()))
+@parameterized.expand([
+    ("SO3", SO3),
+    ("SE3", SE3),
+])
+class TestOperations(parameterized.TestCase):
+    def test_inverse_bijective(self, _, group: Type[MatrixLieGroup]):
+        """Check inverse of inverse."""
+        transform = group.sample_uniform()
+        assert_transforms_close(transform, transform.inverse().inverse())
 
+    def test_matrix_bijective(self, _, group: Type[MatrixLieGroup]):
+        """Check that we can convert to and from matrices."""
+        transform = group.sample_uniform()
+        assert_transforms_close(transform, group.from_matrix(transform.as_matrix()))
 
-def test_log_exp_bijective(group: Type[MatrixLieGroup]):
-    """Check 1-to-1 mapping for log <=> exp operations."""
-    transform = group.sample_uniform()
+    def test_log_exp_bijective(self, _, group: Type[MatrixLieGroup]):
+        """Check 1-to-1 mapping for log <=> exp operations."""
+        transform = group.sample_uniform()
 
-    tangent = transform.log()
-    assert tangent.shape == (group.tangent_dim,)
+        tangent = transform.log()
+        self.assertEqual(tangent.shape, (group.tangent_dim,))
 
-    exp_transform = group.exp(tangent)
-    assert_transforms_close(transform, exp_transform)
-    np.testing.assert_allclose(tangent, exp_transform.log())
+        exp_transform = group.exp(tangent)
+        assert_transforms_close(transform, exp_transform)
+        np.testing.assert_allclose(tangent, exp_transform.log())
 
+    def test_adjoint(self, _, group: Type[MatrixLieGroup]):
+        transform = group.sample_uniform()
+        omega = np.random.randn(group.tangent_dim)
+        assert_transforms_close(
+            transform @ group.exp(omega),
+            group.exp(transform.adjoint() @ omega) @ transform,
+        )
 
-def test_adjoint(group: Type[MatrixLieGroup]):
-    transform = group.sample_uniform()
-    omega = np.random.randn(group.tangent_dim)
-    assert_transforms_close(
-        transform @ group.exp(omega),
-        group.exp(transform.adjoint() @ omega) @ transform,
-    )
+    def test_rminus(self, _, group: Type[MatrixLieGroup]):
+        T_a = group.sample_uniform()
+        T_b = group.sample_uniform()
+        T_c = T_a.inverse() @ T_b
+        np.testing.assert_allclose(T_b.rminus(T_a), T_c.log())
 
+    def test_lminus(self, _, group: Type[MatrixLieGroup]):
+        T_a = group.sample_uniform()
+        T_b = group.sample_uniform()
+        np.testing.assert_allclose(T_a.lminus(T_b), (T_a @ T_b.inverse()).log())
 
-def test_rminus(group: Type[MatrixLieGroup]):
-    T_a = group.sample_uniform()
-    T_b = group.sample_uniform()
-    T_c = T_a.inverse() @ T_b
-    np.testing.assert_allclose(T_b.rminus(T_a), T_c.log())
+    def test_rplus(self, _, group: Type[MatrixLieGroup]):
+        T_a = group.sample_uniform()
+        T_b = group.sample_uniform()
+        T_c = T_a.inverse() @ T_b
+        assert_transforms_close(T_a.rplus(T_c.log()), T_b)
 
+    def test_lplus(self, _, group: Type[MatrixLieGroup]):
+        T_a = group.sample_uniform()
+        T_b = group.sample_uniform()
+        T_c = T_a @ T_b.inverse()
+        assert_transforms_close(T_b.lplus(T_c.log()), T_a)
 
-def test_lminus(group: Type[MatrixLieGroup]):
-    T_a = group.sample_uniform()
-    T_b = group.sample_uniform()
-    np.testing.assert_allclose(T_a.lminus(T_b), (T_a @ T_b.inverse()).log())
-
-
-def test_rplus(group: Type[MatrixLieGroup]):
-    T_a = group.sample_uniform()
-    T_b = group.sample_uniform()
-    T_c = T_a.inverse() @ T_b
-    assert_transforms_close(T_a.rplus(T_c.log()), T_b)
-
-
-def test_lplus(group: Type[MatrixLieGroup]):
-    T_a = group.sample_uniform()
-    T_b = group.sample_uniform()
-    T_c = T_a @ T_b.inverse()
-    assert_transforms_close(T_b.lplus(T_c.log()), T_a)
-
-
-def test_jlog(group: Type[MatrixLieGroup]):
-    state = group.sample_uniform()
-    w = np.random.rand(state.tangent_dim) * 1e-4
-    state_pert = state.plus(w).log()
-    state_lin = state.log() + state.jlog() @ w
-    np.testing.assert_allclose(state_pert, state_lin, atol=1e-7)
+    def test_jlog(self, _, group: Type[MatrixLieGroup]):
+        state = group.sample_uniform()
+        w = np.random.rand(state.tangent_dim) * 1e-4
+        state_pert = state.plus(w).log()
+        state_lin = state.log() + state.jlog() @ w
+        np.testing.assert_allclose(state_pert, state_lin, atol=1e-7)
 
 
 class TestGroupSpecificOperations(absltest.TestCase):
@@ -86,23 +89,6 @@ class TestGroupSpecificOperations(absltest.TestCase):
     def test_so3_rpy_bijective(self):
         T = SO3.sample_uniform()
         assert_transforms_close(T, SO3.from_rpy_radians(*T.as_rpy_radians()))
-
-
-@parameterized.named_parameters(
-    ("SO3", SO3),
-    ("SE3", SE3),
-)
-class TestOperations(parameterized.TestCase):
-    def runTest(self, group: Type[MatrixLieGroup]):
-        test_inverse_bijective(group)
-        test_matrix_bijective(group)
-        test_log_exp_bijective(group)
-        test_adjoint(group)
-        test_rminus(group)
-        test_lminus(group)
-        test_rplus(group)
-        test_lplus(group)
-        test_jlog(group)
 
 
 if __name__ == "__main__":
