677c6b62e3f374257dd82cbb9136dc0e43ca356d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 14:27:55 +0000

    Reset to a69629a7fbeccaa8b60c30e9a385b9c1de01e3cf and made changes for LLM Round 1

--- tests/test_lie_operations.py
+++ tests/test_lie_operations.py
@@ -18,17 +18,17 @@ from .utils import assert_transforms_close
 )
 class TestOperations(parameterized.TestCase):
     def test_inverse_bijective(self, group: Type[MatrixLieGroup]):
-        """Check inverse of inverse."""
+        """Check that the inverse of the inverse is the original transformation."""
         transform = group.sample_uniform()
         assert_transforms_close(transform, transform.inverse().inverse())
 
     def test_matrix_bijective(self, group: Type[MatrixLieGroup]):
-        """Check that we can convert to and from matrices."""
+        """Check that we can convert to and from matrices without loss of information."""
         transform = group.sample_uniform()
         assert_transforms_close(transform, group.from_matrix(transform.as_matrix()))
 
     def test_log_exp_bijective(self, group: Type[MatrixLieGroup]):
-        """Check 1-to-1 mapping for log <=> exp operations."""
+        """Check that the log and exp functions are bijective."""
         transform = group.sample_uniform()
 
         tangent = transform.log()
@@ -39,6 +39,7 @@ class TestOperations(parameterized.TestCase):
         np.testing.assert_allclose(tangent, exp_transform.log())
 
     def test_adjoint(self, group: Type[MatrixLieGroup]):
+        """Check that the adjoint transformation is correctly applied."""
         transform = group.sample_uniform()
         omega = np.random.randn(group.tangent_dim)
         assert_transforms_close(
@@ -47,29 +48,34 @@ class TestOperations(parameterized.TestCase):
         )
 
     def test_rminus(self, group: Type[MatrixLieGroup]):
+        """Check the rminus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         T_c = T_a.inverse() @ T_b
         np.testing.assert_allclose(T_b.rminus(T_a), T_c.log())
 
     def test_lminus(self, group: Type[MatrixLieGroup]):
+        """Check the lminus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         np.testing.assert_allclose(T_a.lminus(T_b), (T_a @ T_b.inverse()).log())
 
     def test_rplus(self, group: Type[MatrixLieGroup]):
+        """Check the rplus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         T_c = T_a.inverse() @ T_b
         assert_transforms_close(T_a.rplus(T_c.log()), T_b)
 
     def test_lplus(self, group: Type[MatrixLieGroup]):
+        """Check the lplus operation."""
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         T_c = T_a @ T_b.inverse()
         assert_transforms_close(T_b.lplus(T_c.log()), T_a)
 
     def test_jlog(self, group: Type[MatrixLieGroup]):
+        """Check the jlog operation."""
         state = group.sample_uniform()
         w = np.random.rand(state.tangent_dim) * 1e-4
         state_pert = state.plus(w).log()
@@ -81,6 +87,7 @@ class TestGroupSpecificOperations(absltest.TestCase):
     """Group specific tests."""
 
     def test_so3_rpy_bijective(self):
+        """Check that SO3 transformations can be converted to and from RPY angles."""
         T = SO3.sample_uniform()
         assert_transforms_close(T, SO3.from_rpy_radians(*T.as_rpy_radians()))
 
