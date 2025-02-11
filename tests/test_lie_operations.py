3cdb1a652646843028983df191289e58e888d91e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:29:29 +0000

    Reset to a69629a7fbeccaa8b60c30e9a385b9c1de01e3cf and made changes for LLM Round 1

--- tests/test_lie_operations.py
+++ tests/test_lie_operations.py
@@ -4,7 +4,6 @@ from typing import Type
 
 import numpy as np
 from absl.testing import absltest, parameterized
-import pytest
 
 from mink.lie.base import MatrixLieGroup
 from mink.lie.se3 import SE3
@@ -13,27 +12,22 @@ from mink.lie.so3 import SO3
 from .utils import assert_transforms_close
 
 
-@pytest.fixture(params=[SO3, SE3])
-def group(request):
-    return request.param
-
-
-@parameterized.expand([
+@parameterized.named_parameters(
     ("SO3", SO3),
     ("SE3", SE3),
-])
+)
 class TestOperations(parameterized.TestCase):
-    def test_inverse_bijective(self, _, group: Type[MatrixLieGroup]):
+    def test_inverse_bijective(self, group: Type[MatrixLieGroup]):
         """Check inverse of inverse."""
         transform = group.sample_uniform()
         assert_transforms_close(transform, transform.inverse().inverse())
 
-    def test_matrix_bijective(self, _, group: Type[MatrixLieGroup]):
+    def test_matrix_bijective(self, group: Type[MatrixLieGroup]):
         """Check that we can convert to and from matrices."""
         transform = group.sample_uniform()
         assert_transforms_close(transform, group.from_matrix(transform.as_matrix()))
 
-    def test_log_exp_bijective(self, _, group: Type[MatrixLieGroup]):
+    def test_log_exp_bijective(self, group: Type[MatrixLieGroup]):
         """Check 1-to-1 mapping for log <=> exp operations."""
         transform = group.sample_uniform()
 
@@ -44,7 +38,7 @@ class TestOperations(parameterized.TestCase):
         assert_transforms_close(transform, exp_transform)
         np.testing.assert_allclose(tangent, exp_transform.log())
 
-    def test_adjoint(self, _, group: Type[MatrixLieGroup]):
+    def test_adjoint(self, group: Type[MatrixLieGroup]):
         transform = group.sample_uniform()
         omega = np.random.randn(group.tangent_dim)
         assert_transforms_close(
@@ -52,30 +46,30 @@ class TestOperations(parameterized.TestCase):
             group.exp(transform.adjoint() @ omega) @ transform,
         )
 
-    def test_rminus(self, _, group: Type[MatrixLieGroup]):
+    def test_rminus(self, group: Type[MatrixLieGroup]):
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         T_c = T_a.inverse() @ T_b
         np.testing.assert_allclose(T_b.rminus(T_a), T_c.log())
 
-    def test_lminus(self, _, group: Type[MatrixLieGroup]):
+    def test_lminus(self, group: Type[MatrixLieGroup]):
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         np.testing.assert_allclose(T_a.lminus(T_b), (T_a @ T_b.inverse()).log())
 
-    def test_rplus(self, _, group: Type[MatrixLieGroup]):
+    def test_rplus(self, group: Type[MatrixLieGroup]):
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         T_c = T_a.inverse() @ T_b
         assert_transforms_close(T_a.rplus(T_c.log()), T_b)
 
-    def test_lplus(self, _, group: Type[MatrixLieGroup]):
+    def test_lplus(self, group: Type[MatrixLieGroup]):
         T_a = group.sample_uniform()
         T_b = group.sample_uniform()
         T_c = T_a @ T_b.inverse()
         assert_transforms_close(T_b.lplus(T_c.log()), T_a)
 
-    def test_jlog(self, _, group: Type[MatrixLieGroup]):
+    def test_jlog(self, group: Type[MatrixLieGroup]):
         state = group.sample_uniform()
         w = np.random.rand(state.tangent_dim) * 1e-4
         state_pert = state.plus(w).log()
