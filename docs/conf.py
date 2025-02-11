839df1a726e97b9f903e37e3e6c94192c3faa592
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:14:40 +0000

    Reset to c9885f28a9f81f574c4cb89778f23937ac93622d and made changes for LLM Round 1

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,38 @@
-project = "mink"
-copyright = "2024, Kevin Zakka"
-author = "Kevin Zakka"
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx-mathjax-offline", "sphinx.ext.napoleon", "sphinx_favicon"]
+# -- Project information -----------------------------------------------------
+# Retrieve project information from pyproject.toml
+import toml
+
+with open("pyproject.toml", "r") as file:
+    pyproject = toml.load(file)
+
+project = pyproject["project"]["name"]
+copyright = "2024, Kevin Zakka"
+author = pyproject["project"]["authors"][0]["name"]
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx-mathjax-offline",
+    "sphinx.ext.napoleon",
+    "sphinx_favicon",
+]
 
 autodoc_typehints = "both"
 autodoc_class_signature = "separated"
-autodoc_default_options = {"members": True, "member-order": "bysource", "inherited-members": False, "exclude-members": "__init__, __post_init__, __new__"}
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "inherited-members": False,
+    "exclude-members": "__init__, __post_init__, __new__",
+}
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -15,7 +41,12 @@ source_suffix = {".rst": "restructuredtext"}
 
 pygments_style = "sphinx"
 
-autodoc_type_aliases = {"npt.ArrayLike": "ArrayLike"}
+autodoc_type_aliases = {
+    "npt.ArrayLike": "ArrayLike",
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 
