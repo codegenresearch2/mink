b17713c03fa0eaa494a263e3fc053922e45b98c3
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 14:02:18 +0000

    Reset to 372c0d0807895860f5f90ac4cc1d0cae7b98f14c and made changes for LLM Round 1

--- tests/test_lie_utils.py
+++ tests/test_lie_utils.py
@@ -11,11 +11,6 @@ class TestUtils(absltest.TestCase):
         with self.assertRaises(ValueError):
             utils.skew(np.zeros((5,)))
 
-    def test_skew_equals_negative(self):
-        vec = np.random.randn(3)
-        skew_matrix = utils.skew(vec)
-        np.testing.assert_allclose(skew_matrix.T, -skew_matrix)
-
     def test_skew_with_valid_input(self):
         vec = np.array([1, 2, 3])
         expected_skew = np.array([[0, -3, 2], [3, 0, -1], [-2, 1, 0]])
@@ -25,16 +20,6 @@ class TestUtils(absltest.TestCase):
         vec = np.zeros(3)
         expected_skew = np.zeros((3, 3))
         np.testing.assert_allclose(utils.skew(vec), expected_skew)
-
-    def test_skew_with_large_input(self):
-        vec = np.random.randn(10)
-        with self.assertRaises(ValueError):
-            utils.skew(vec)
-
-    def test_skew_with_negative_input(self):
-        vec = -np.random.randn(3)
-        skew_matrix = utils.skew(vec)
-        np.testing.assert_allclose(skew_matrix.T, -skew_matrix)
 
 
 if __name__ == "__main__":
