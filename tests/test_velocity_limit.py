f454fd6b6028b1da81fd71f3c9f7c8ad58431751
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 14:24:59 +0000

    Reset to e861235d28d5598fac82c90aaec11c9d4218cc3c and made changes for LLM Round 1

--- tests/test_velocity_limit.py
+++ tests/test_velocity_limit.py
@@ -19,8 +19,7 @@ class TestVelocityLimit(absltest.TestCase):
 
     def setUp(self):
         self.configuration = Configuration(self.model)
-        self.configuration.update_from_keyframe("home")  # Changed to "home" as per feedback
-        # NOTE(kevin): These velocities are arbitrary and do not match real hardware.
+        self.configuration.update_from_keyframe("stand")  # Updated to "stand"
         self.velocities = {
             self.model.joint(i).name: 3.14 for i in range(1, self.model.njnt)
         }
