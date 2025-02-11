c9ca727444a0cb097d35a1d00d97b0b97d7d2e4a
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 12:36:47 +0000

    Reset to a69629a7fbeccaa8b60c30e9a385b9c1de01e3cf and made changes for LLM Round 1

--- tests/test_lie_operations.py
+++ tests/test_lie_operations.py
@@ -2,16 +2,87 @@
 
 from typing import Type
 
-import mujoco
 import numpy as np
 from absl.testing import absltest, parameterized
 
-from mink.exceptions import InvalidMocapBody
 from mink.lie.base import MatrixLieGroup
 from mink.lie.se3 import SE3
 from mink.lie.so3 import SO3
 
 from .utils import assert_transforms_close
+
+
+def check_inverse_bijective(group: Type[MatrixLieGroup]):
+    """Check inverse of inverse."""
+    transform = group.sample_uniform()
+    assert_transforms_close(transform, transform.inverse().inverse())
+
+
+def check_matrix_bijective(group: Type[MatrixLieGroup]):
+    """Check that we can convert to and from matrices."""
+    transform = group.sample_uniform()
+    assert_transforms_close(transform, group.from_matrix(transform.as_matrix()))
+
+
+def check_log_exp_bijective(group: Type[MatrixLieGroup]):
+    """Check 1-to-1 mapping for log <=> exp operations."""
+    transform = group.sample_uniform()
+
+    tangent = transform.log()
+    self.assertEqual(tangent.shape, (group.tangent_dim,))
+
+    exp_transform = group.exp(tangent)
+    assert_transforms_close(transform, exp_transform)
+    np.testing.assert_allclose(tangent, exp_transform.log())
+
+
+def check_adjoint(group: Type[MatrixLieGroup]):
+    transform = group.sample_uniform()
+    omega = np.random.randn(group.tangent_dim)
+    assert_transforms_close(
+        transform @ group.exp(omega),
+        group.exp(transform.adjoint() @ omega) @ transform,
+    )
+
+
+def check_rminus(group: Type[MatrixLieGroup]):
+    T_a = group.sample_uniform()
+    T_b = group.sample_uniform()
+    T_c = T_a.inverse() @ T_b
+    np.testing.assert_allclose(T_b.rminus(T_a), T_c.log())
+
+
+def check_lminus(group: Type[MatrixLieGroup]):
+    T_a = group.sample_uniform()
+    T_b = group.sample_uniform()
+    np.testing.assert_allclose(T_a.lminus(T_b), (T_a @ T_b.inverse()).log())
+
+
+def check_rplus(group: Type[MatrixLieGroup]):
+    T_a = group.sample_uniform()
+    T_b = group.sample_uniform()
+    T_c = T_a.inverse() @ T_b
+    assert_transforms_close(T_a.rplus(T_c.log()), T_b)
+
+
+def check_lplus(group: Type[MatrixLieGroup]):
+    T_a = group.sample_uniform()
+    T_b = group.sample_uniform()
+    T_c = T_a @ T_b.inverse()
+    assert_transforms_close(T_b.lplus(T_c.log()), T_a)
+
+
+def check_jlog(group: Type[MatrixLieGroup]):
+    state = group.sample_uniform()
+    w = np.random.rand(state.tangent_dim) * 1e-4
+    state_pert = state.plus(w).log()
+    state_lin = state.log() + state.jlog() @ w
+    np.testing.assert_allclose(state_pert, state_lin, atol=1e-7)
+
+
+def check_so3_rpy_bijective():
+    T = SO3.sample_uniform()
+    assert_transforms_close(T, SO3.from_rpy_radians(*T.as_rpy_radians()))
 
 
 @parameterized.named_parameters(
@@ -20,140 +91,38 @@ from .utils import assert_transforms_close
 )
 class TestOperations(parameterized.TestCase):
     def test_inverse_bijective(self, group: Type[MatrixLieGroup]):
-        """Check inverse of inverse."""
-        transform = group.sample_uniform()
-        assert_transforms_close(transform, transform.inverse().inverse())
+        check_inverse_bijective(group)
 
     def test_matrix_bijective(self, group: Type[MatrixLieGroup]):
-        """Check that we can convert to and from matrices."""
-        transform = group.sample_uniform()
-        assert_transforms_close(transform, group.from_matrix(transform.as_matrix()))
+        check_matrix_bijective(group)
 
     def test_log_exp_bijective(self, group: Type[MatrixLieGroup]):
-        """Check 1-to-1 mapping for log <=> exp operations."""
-        transform = group.sample_uniform()
-
-        tangent = transform.log()
-        self.assertEqual(tangent.shape, (group.tangent_dim,))
-
-        exp_transform = group.exp(tangent)
-        assert_transforms_close(transform, exp_transform)
-        np.testing.assert_allclose(tangent, exp_transform.log())
+        check_log_exp_bijective(group)
 
     def test_adjoint(self, group: Type[MatrixLieGroup]):
-        transform = group.sample_uniform()
-        omega = np.random.randn(group.tangent_dim)
-        assert_transforms_close(
-            transform @ group.exp(omega),
-            group.exp(transform.adjoint() @ omega) @ transform,
-        )
+        check_adjoint(group)
 
     def test_rminus(self, group: Type[MatrixLieGroup]):
-        T_a = group.sample_uniform()
-        T_b = group.sample_uniform()
-        T_c = T_a.inverse() @ T_b
-        np.testing.assert_allclose(T_b.rminus(T_a), T_c.log())
+        check_rminus(group)
 
     def test_lminus(self, group: Type[MatrixLieGroup]):
-        T_a = group.sample_uniform()
-        T_b = group.sample_uniform()
-        np.testing.assert_allclose(T_a.lminus(T_b), (T_a @ T_b.inverse()).log())
+        check_lminus(group)
 
     def test_rplus(self, group: Type[MatrixLieGroup]):
-        T_a = group.sample_uniform()
-        T_b = group.sample_uniform()
-        T_c = T_a.inverse() @ T_b
-        assert_transforms_close(T_a.rplus(T_c.log()), T_b)
+        check_rplus(group)
 
     def test_lplus(self, group: Type[MatrixLieGroup]):
-        T_a = group.sample_uniform()
-        T_b = group.sample_uniform()
-        T_c = T_a @ T_b.inverse()
-        assert_transforms_close(T_b.lplus(T_c.log()), T_a)
+        check_lplus(group)
 
     def test_jlog(self, group: Type[MatrixLieGroup]):
-        state = group.sample_uniform()
-        w = np.random.rand(state.tangent_dim) * 1e-4
-        state_pert = state.plus(w).log()
-        state_lin = state.log() + state.jlog() @ w
-        np.testing.assert_allclose(state_pert, state_lin, atol=1e-7)
+        check_jlog(group)
 
 
 class TestGroupSpecificOperations(absltest.TestCase):
     """Group specific tests."""
 
-    # SO3.
-
     def test_so3_rpy_bijective(self):
-        T = SO3.sample_uniform()
-        assert_transforms_close(T, SO3.from_rpy_radians(*T.as_rpy_radians()))
-
-    def test_so3_raises_error_if_invalid_shape(self):
-        with self.assertRaises(ValueError):
-            SO3(wxyz=np.random.rand(2))
-
-    def test_so3_copy(self):
-        T = SO3.sample_uniform()
-        T_c = T.copy()
-        np.testing.assert_allclose(T_c.wxyz, T.wxyz)
-        T.wxyz[0] = 1.0
-        with np.testing.assert_raises(AssertionError):
-            np.testing.assert_allclose(T_c.wxyz, T.wxyz)
-
-    # SE3.
-
-    def test_se3_from_mocap_id(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body name="mocap" mocap="true" pos=".5 1 5" quat="1 1 0 0">
-              <geom type="sphere" size=".1" mass=".1"/>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
-        model = mujoco.MjModel.from_xml_string(xml_str)
-        data = mujoco.MjData(model)
-        mujoco.mj_forward(model, data)
-
-        mid = model.body("mocap").mocapid[0]
-        pose = SE3.from_mocap_id(data, mocap_id=mid)
-        np.testing.assert_allclose(pose.translation(), data.mocap_pos[mid])
-        np.testing.assert_allclose(pose.rotation().wxyz, data.mocap_quat[mid])
-
-    def test_se3_from_mocap_name(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body name="mocap" mocap="true" pos=".5 1 5" quat="1 1 0 0">
-              <geom type="sphere" size=".1" mass=".1"/>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
-        model = mujoco.MjModel.from_xml_string(xml_str)
-        data = mujoco.MjData(model)
-        mujoco.mj_forward(model, data)
-
-        pose = SE3.from_mocap_name(model, data, "mocap")
-        mid = model.body("mocap").mocapid[0]
-        np.testing.assert_allclose(pose.translation(), data.mocap_pos[mid])
-        np.testing.assert_allclose(pose.rotation().wxyz, data.mocap_quat[mid])
-
-    def test_se3_from_mocap_name_raises_error_if_body_not_mocap(self):
-        xml_str = """
-        <mujoco>
-          <worldbody>
-            <body name="test" pos=".5 1 5" quat="1 1 0 0">
-              <geom type="sphere" size=".1" mass=".1"/>
-            </body>
-          </worldbody>
-        </mujoco>
-        """
-        model = mujoco.MjModel.from_xml_string(xml_str)
-        data = mujoco.MjData(model)
-        with self.assertRaises(InvalidMocapBody):
-            SE3.from_mocap_name(model, data, "test")
+        check_so3_rpy_bijective()
 
 
 if __name__ == "__main__":
