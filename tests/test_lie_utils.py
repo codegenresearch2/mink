18e9aedf0bd498cd326b6cc21198655a9e92fe7e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 13:00:50 +0000

    Reset to 372c0d0807895860f5f90ac4cc1d0cae7b98f14c and made changes for LLM Round 1

--- tests/test_lie_utils.py
+++ tests/test_lie_utils.py
@@ -12,8 +12,9 @@ class TestUtils(absltest.TestCase):
             utils.skew(np.zeros((5,)))
 
     def test_skew_equals_negative(self):
-        m = utils.skew(np.random.randn(3))
-        np.testing.assert_allclose(m.T, -m)
+        vec = np.random.randn(3)
+        skew_matrix = utils.skew(vec)
+        np.testing.assert_allclose(skew_matrix.T, -skew_matrix)
 
 
 if __name__ == "__main__":
